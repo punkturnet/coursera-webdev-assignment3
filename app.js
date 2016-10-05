39217fe0d685b311e7cfbda0c79a100d81a78f87
Geiri <git@geiri.net>
2016-10-05 20:42:08 +0000

    ...

---
+++ app.js
@@ -0,0 +1,56 @@
+(function(){
+  'use strict';
+  angular.module("NarrowItDownApp", [])
+  .controller("NarrowItDownController", NarrowItDownController)
+  .service("MenuService", MenuService)
+  .directive("FoundItemsDirective", FoundItemsDirective);
+
+  function FoundItemsDirective(){
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope : {
+        foundItems : '<',
+        onRemove : '&'
+      }
+      //,controller: FoundItemsDirectiveController,
+      //controllerAs: list,
+      //bindToController: true
+    };
+    return ddo;
+  }
+
+  NarrowItDownController.$inject = ["MenuService"];
+  function NarrowItDownController(MenuService){
+    var narrow = this;
+    narrow.found = [];
+    narrow.searchTerm = "";
+
+    narrow.search = function(){
+      narrow.found = MenuService.getMatchedMenuItems(this.searchTerm);
+    };
+
+    narrow.removeItem = function(itemIndex){
+      narrow.found.splice(itemIndex, 1);
+    };
+  }
+
+  function MenuService() {
+    var service = this;
+
+    service.getMatchedMenuItems = function (searchTerm) {
+      return $http({
+        url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+      })
+      .then(function(result){
+        var foundItems = [];
+        console.log(result.data)
+        //for(var i = 0; i < )
+        //if (name.toLowerCase().indexOf("cookie") !== -1) {
+        //  return true;
+        //}
+        return foundItems;
+      })
+    };
+  }
+
+})();
